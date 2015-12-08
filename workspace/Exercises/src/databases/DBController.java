fd9ad113b3fec9bb30a59a7fbda0740c042a5c41
AARONT59570 <athompson.59570@farnborough.ac.uk>
2015-12-08 10:20:20 +0000

    End Of Lesson 08 12 2015
    08/12/2015

---
+++ workspace/Exercises/src/databases/DBController.java
@@ -0,0 +1,12 @@
+package databases;
+
+public class DBController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DBEdit.init();
+		DBEdit.displayRecords();
+
+	}
+
+}
--- workspace/Exercises/src/databases/DBEdit.java
+++ workspace/Exercises/src/databases/DBEdit.java
@@ -1,36 +1,54 @@
 package databases;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 public class DBEdit {
-	String csvFile = "/Users/CompUser/Documents/records.csv";
-	BufferedReader br = null;
-	String line = "";
-	String cvsSplitBy = ",";
-	br = new BufferedReader(new FileReader(csvFile));
-	try {
-		while ((line = br.readLine()) != null) {
-	
-		        // use comma as separator
-			String[] recordStr = line.split(cvsSplitBy);
-			Record record = new Record(recordStr[0], recordStr[1], recordStr[2], recordStr[3]);
-		}
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException b) {
-		b.printStackTrace();
-	} finally {
-		if (br != null) {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	private static List<Record> records = new ArrayList<Record>();
+	public static void init() {
+		String csvFilePath = "c:\\Users\\CompUser\\Documents\\records.csv";
+		File csvFile = null;
+		try {
+			csvFile = new File(csvFilePath);
+			if(csvFile.createNewFile())
+			{
+				System.out.println("New CSV File Created");
 			}
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(csvFile));
+			br.readLine();
+			String line;
+			String[] row;
+			while((line = br.readLine()) != null)
+			{
+				row = line.split(",");
+				Record r = new Record(row[0], row[1], row[2], row[3]);
+				records.add(r);
+			}
+		} catch (IOException e)
+		{ 
+			e.printStackTrace();
 		}
 	}
-
-
+	public static void displayRecords()
+	{
+		for (Iterator iterator = records.iterator(); iterator.hasNext();) {
+			Record record = (Record) iterator.next();
+			record.displayRecord();
+			System.out.println("");
+		}
+	}
+	
 }
